eefbf27949ce4c3cb3470d44884f5d5e725bddd9
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-04-23 12:19:54 -0400

    feat: add exponential error backoff to DataCache

--- src/webparts/quickSparksHub/services/DataCache.ts
+++ src/webparts/quickSparksHub/services/DataCache.ts
@@ -3,6 +3,8 @@ export class DataCache<T> {
     private _fetchedAt: number = 0;
     private _ttlMs: number;
     private _pendingFetch: Promise<T> | null = null;
+    private _errorBackoffUntil: number = 0;
+    private _consecutiveErrors: number = 0;
 
     constructor(ttlMs: number = 5 * 60 * 1000) {
         this._ttlMs = ttlMs;
@@ -13,16 +15,25 @@ export class DataCache<T> {
             return this._data;
         }
 
+        if (Date.now() < this._errorBackoffUntil) {
+            throw new Error('Data fetch failed recently. Retrying shortly.');
+        }
+
         if (!this._pendingFetch) {
             this._pendingFetch = fetcher()
                 .then((data) => {
                     this._data = data;
                     this._fetchedAt = Date.now();
                     this._pendingFetch = null;
+                    this._consecutiveErrors = 0;
+                    this._errorBackoffUntil = 0;
                     return data;
                 })
                 .catch((err) => {
                     this._pendingFetch = null;
+                    this._consecutiveErrors++;
+                    const backoffMs = Math.min(1000 * 2 ** this._consecutiveErrors, 30000);
+                    this._errorBackoffUntil = Date.now() + backoffMs;
                     throw err;
                 });
         }
